f90f7ac889bb290a5f7c5df7b0d8a95053dcd8a8
deviloper <edmundjryan@gmail.com>
2022-10-05 17:44:22 -0400

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { IntroComponent } from './intro/intro.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent,
+    IntroComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
