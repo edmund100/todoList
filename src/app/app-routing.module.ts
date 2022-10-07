d33a17a368cae1f842a39bed1b8d04ce3b1d4ac9
deviloper <edmundjryan@gmail.com>
2022-10-06 23:22:02 -0400

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { IntroComponent } from './intro/intro.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { TasksComponent } from './tasks/tasks.component';
 
+const routes: Routes = [
+  { path: '', component: IntroComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'tasks', component: TasksComponent }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
