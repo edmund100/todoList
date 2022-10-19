b251e82eb0003f11d13ef663191d65dbd4aa5e16
deviloper <edmundjryan@gmail.com>
2022-10-19 10:55:13 -0400

    changes

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -10,35 +10,48 @@ export class TasksComponent implements OnInit {
   // create an internal variable (do not change manually)
   private _taskDescription?:string;
 
+  public Tasks?:Task[];
+
   // getter function, called whenever the value is accessed
   get taskDescription(){
     return this._taskDescription;
   }
 
+  deleteAllTasks() {
+    this.Tasks = new Array<Task>();
+    localStorage.setItem("Tasks", JSON.stringify(this.Tasks));
+  }
+
+  createTask(){
+    const taskDescription = this._taskDescription;
+    const newTask = new Task();
+    newTask.Description = taskDescription;
+
+    if (!this.Tasks){
+      this.Tasks = new Array<Task>();
+    }
+
+    const previousTask = this.Tasks.find((element,index)=>{
+      return element.Description == taskDescription;
+    })
+
+    if (!previousTask){
+      this.Tasks.push(newTask);
+      localStorage.setItem("Tasks", JSON.stringify(this.Tasks));  
+    }
+  }
+
   // setter function, called whenever the value is set
   set taskDescription(text){
     this._taskDescription = text;
-
-    let newTask = new Task();
-    newTask.Description = text;
-
-    let newTasks = new Array<Task>();
-    newTasks.push(newTask);
-
-    localStorage.setItem("Tasks", JSON.stringify(newTasks));
   }
 
   constructor() {
     let jsonTasks = String(localStorage.getItem("Tasks"));
 
     let tasks = JSON.parse(jsonTasks) as Task[];
-    if (tasks)
-    {
-      this._taskDescription = tasks[0].Description?.toString();  
-    }
-    else{
-      this._taskDescription= "";
-    }
+
+    this.Tasks = tasks;
    }
 
   ngOnInit(): void {
